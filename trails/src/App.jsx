e65cf7a2db7e6e2f6a0fa797aa728a3f6b5285fd
franciscogrimes <fgsieg1831@gmail.com>
2024-04-17 08:17:31 -0300

    Inserção do Footer

--- trails/src/App.jsx
+++ trails/src/App.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import Header from "./components/CardTrilha/header";
+import Footer from "./components/CardTrilha/footer";
 import { Outlet } from "react-router-dom";
 import { TrilhasContextProvider } from "./context/TrilhasContext";
 
@@ -8,6 +9,7 @@ function App() {
     <TrilhasContextProvider>
       <Header />
       <Outlet />
+      <Footer />
     </TrilhasContextProvider>
   );
 }
