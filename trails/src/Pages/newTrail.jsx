e1e91d5aa20416a7180312c3831c883b432588e1
franciscogrimes <fgsieg1831@gmail.com>
2024-04-19 13:43:59 -0300

    feat: 🎸 Alterações na Tela de Cadastro de trilhas

--- trails/src/Pages/newTrail.jsx
+++ trails/src/Pages/newTrail.jsx
@@ -1,12 +1,33 @@
-import {useForm} from "react-hook-form";
+import React from 'react';
+
+import { useForm } from "react-hook-form";
 import Button from '@mui/material/Button';
+import InputLabel from '@mui/material/InputLabel';
+import MenuItem from '@mui/material/MenuItem';
+import FormControl from '@mui/material/FormControl';
+import Select from '@mui/material/Select';
+import Box from '@mui/material/Box';
+import TextField from '@mui/material/TextField';
+
 
 
 function newTrail() {
-
+  const { register, handleSubmit, formState: { errors }} = useForm();
   
-  const { register, handleSubmit, formState: {errors}} = useForm();
-  const onSubmit = ( dados ) => console . registro ( dados )
+  const onSubmit = (dados) => console.registro(dados);
+
+  const [difficulty, setDifficulty] = React.useState('');
+  const [type, setType] = React.useState('');
+
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+    if (name === "difficulty") {
+      setDifficulty(value);
+    } else if (name === "type") {
+      setType(value);
+    }
+  };
+
 
   return (
     <>
@@ -15,99 +36,132 @@ function newTrail() {
         Abaixo, preencha os dados para o cadastro da nova trilha na plataforma:
       </p>
       <form onSubmit={handleSubmit(onSubmit)}>
-         <div>
-          <label htmlFor="name">Nome completo:</label>
-         <input 
-            type="text"
-            {...register("name",{
-              required: "Por favor insira seu nome.",
-              minLength:{
-                value: 5,
-                message: "Digite ao menos 5 caracteres."
-              },
-              pattern:{
-                value: 60,
-                message: "Ops...Você atingiu o limite máximo de 60 caracteres."
-              }
-            })}
-            />
-            {errors.name && <p>{errors.name.message}</p>}
-        
-          
-         </div>           
         <div>
-          <label htmlFor="nameTrail" id="nameTrail">
-            Nome da trilha:
-          </label>
-          <input
-            type="text"
+
+          <TextField
+          required
+          id="outlined-required"
+          label="Nome Completo"
+          {...register("name", {
+            required: "Por favor insira seu nome.",
+            minLength: {
+              value: 5,
+              message: "Digite ao menos 5 caracteres."
+            },
+            pattern: {
+              value: 60,
+              message: "Ops...Você atingiu o limite máximo de 60 caracteres."
+            }})}
+        />
+            {errors.name && <p>{errors.name.message}</p>}
+      
+        </div>
+
+        <div>
+        <TextField
+          required
+          id="outlined-required"
+          label="Nome da Trilha"
+          type="text"
             {...register("nameTrail", {
               required: "Por favor, insira o nome da trilha.",
-              minLenght: {
+              minLength: {
                 value: 5,
                 message: "O nome deve conter ao menos 5 caracteres.",
               },
               pattern: {
                 value: 60,
                 message: "Ops...Você atingiu o limite máximo de 60 caracteres.",
-              },
-            })}
-          />
-        </div>
-        <div>
-          <label htmlFor="duration" id="duration">
-            Duração estimada (min):
-          </label>
-          <input type="number" />
-        </div>
-        <div>
-          <label htmlFor="distancy" id="distancy">
-            Distância aproximada (km):
-          </label>
-          <input type="number" />
-        </div>
-        <div>
-          <label htmlFor="city" id="city">
-            Cidade:
-          </label>
-          <input
-            type="text"
-            {...register("nameTrail", {
-              required: "Por favor, insira a cidade onde se encontra essa trilha.",
-              minLenght: {
-                value: 5,
-                message: "O nome deve conter ao menos 5 caracteres.",
-              },
-              pattern: {
-                value: 60,
-                message: "Ops...Você atingiu o limite máximo de 60 caracteres.",
-              },
-            })}
-          />
+            }})}
+        />
+            {errors.name && <p>{errors.nameTrail.message}</p>}
+      
 
         </div>
+
         <div>
-          <label htmlFor="dificulty" id="dificulty">
-            Dificuldade:
-          </label>
-          <select {...register("dificulty", { required: true })}>
-            <option value="high">Alta</option>
-            <option value="medium">Média</option>
-            <option value="low">Baixa</option>
-          </select>
+        <TextField
+          id="outlined-number"
+          label="Duração estimada (min)"
+          type="number"
+          InputLabelProps={{
+            shrink: true,
+          }}
+        />
         </div>
+
         <div>
-          <label htmlFor="type" id="type">
-            Tipo:
-          </label>
-          <select {...register("type", { required: true })}>
-            <option value="Alta">Caminhada</option>
-            <option value="Alta">Trekking</option>
-            <option value="Média">Running</option>
-            <option value="Média">Caminhada/Trekking</option>
-            
-          </select>
+        <TextField
+          id="outlined-number"
+          label="Distância aproximada (km)"
+          type="number"
+          InputLabelProps={{
+            shrink: true,
+          }}
+        />
         </div>
+
+        <div>
+        <TextField
+          required
+          id="outlined-required"
+          label="Cidade"
+          {...register("city", {
+            required: "Por favor, insira a cidade onde se encontra essa trilha.",
+            minLength: {
+              value: 5,
+              message: "O nome da cidade deve conter ao menos 5 caracteres.",
+            },
+            pattern: {
+              value: 60,
+              message: "Ops...Você atingiu o limite máximo de 60 caracteres.",
+            }})}
+        />
+          
+          {errors.city && <p>{errors.city.message}</p>}
+        </div>
+
+        <div>
+          <FormControl>
+            <InputLabel id="difficulty-label">Dificuldade</InputLabel>
+            <Select
+              labelId="difficulty-label"
+              id="difficulty"
+              value={difficulty}
+              name="difficulty"
+              onChange={handleChange}
+            >
+              <MenuItem value="">
+                <em>Nenhuma</em>
+              </MenuItem>
+              <MenuItem value="high">Alta</MenuItem>
+              <MenuItem value="medium">Média</MenuItem>
+              <MenuItem value="low">Baixa</MenuItem>
+            </Select>
+          </FormControl>
+        </div>
+
+        <div>
+        <FormControl>
+            <InputLabel id="type-label">Tipo</InputLabel>
+            <Select
+              labelId="type-label"
+              id="type"
+              value={type}
+              name="type"
+              onChange={handleChange}
+            >
+              <MenuItem value="">
+                <em>Nenhuma</em>
+              </MenuItem>
+              <MenuItem value="caminhada">Caminhada</MenuItem>
+              <MenuItem value="running">Running</MenuItem>
+              <MenuItem value="trekking">Trekking</MenuItem>
+              <MenuItem value="caminhada/trekking">Caminhada/Trekking</MenuItem>
+            </Select>
+          </FormControl>
+        </div>
+
         <div>
           <label htmlFor="image" id="image">
             Imagem:
@@ -115,9 +169,9 @@ function newTrail() {
           <input type="url" />
         </div>
 
-          <Button type="submit" variant="contained" size="medium" > 
-            Cadastrar Trilha
-          </Button>
+        <Button type="submit" variant="contained" size="medium">
+          Cadastrar Trilha
+        </Button>
       </form>
     </>
   );
